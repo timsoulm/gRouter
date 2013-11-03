53665236533a043ea55088cb195a0f19cde9c32c
AdamLevine <a.bednarz.levine@gmail.com>
2013-11-02 21:52:51 -0400

    Initial Commit

---
+++ src/grouter/ospf.c
@@ -0,0 +1,117 @@
+/*
+ * 
+ * OSPF
+ * 
+ */
+
+#include "ospf.h"
+#include "message.h"
+#include "grouter.h"
+#include "gnet.h"
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int create_ls_update()
+{
+    ospf_lsupdate packet;
+
+    packet->ads = malloc(sizeof(NumOfInterfaces*ospf_ad))
+    for(i = 0; i < NumOfInterfaces; i++)
+    {
+        ads[i]->Link = NetworkAddress;
+        if(any2any)
+        {
+
+            ads{i}->data = routeraddress;
+            ads[i]->type = 2;
+        }
+        else
+        {
+            ads{i}->data = NetworkMask;
+            ads[i]->type = 3;
+        }
+    }
+
+
+}
+
+int create_common_header(ospf_header_common* header,
+                         char type,
+                         short length)
+{
+    header-> type = type;
+    header-> msg_length = length;
+    header-> source_ip_addr = NULL; // ???
+}
+
+int unpack_common_header(ospf_header_common* header)
+{
+    int status = 0;
+
+    switch(header->type)
+    case 1:
+        unpack_hello_message();
+        break;
+    case 3:
+        unpack_ls_request();
+        break;
+    case 4:
+        unpack_ls_update();
+        break;
+    default:
+        printf("Invalid OSPF Header Type\n");
+        status = 1;
+
+    return status;
+}
+
+int create_lsa_header()
+{
+
+}
+
+int create_hello_packet(ospf_hello* hello_packet,
+			int* neighbor_list_start)
+{
+	hello_packet-> network_mask = 0xFFFFFF00; //255.255.255.0
+	hello_packet-> hello_interval = 10; //10 seconds
+	hello_packet-> options = 0; //figure out later
+	hello_packet-> priority = 0; //0
+	hello_packet-> 40; //40 seconds
+	hello_packet-> neighbor_list_start = neighbor_list_start;
+
+}
+
+void ospf_init()
+{
+	pthread_t tid;
+	pthread_create(&tid, NULL, &hello_message_thread, NULL);
+}
+
+void *hello_message_thread(void *arg)
+{
+	int* neighbors;
+	gpacket_t *out_pkt;
+	ip_packet_t *ipkt;
+	ospf_hello *hello_packet;
+    while(1)
+    {
+    	neighbors = getInterfaceIPs();
+    	out_pkt = (gpacket_t *) malloc(sizeof(gpacket_t));
+    	ipkt = (ip_packet_t *)(out_pkt->data.data);
+    	ipkt->ip_hdr_len = 5;
+    	hello_packet = (ospf_hello *)((uchar *)ipkt + ipkt->ip_hdr_len*4);
+    	create_hello_packet(hello_packet, neighbors);
+    	
+    	//ip header+ospf header+ospf packet info+payload
+    	IPBroadcastPacket(out_pkt, ipkt->ip_hdr_len*4 + 44 + sizeof(neighbors));
+    	
+    	//wait for 10 seconds before going again
+    	sleep(10);
+    }
+    return 0;
+}
+
---
+++ src/grouter/ospf.h
@@ -0,0 +1,54 @@
+typedef struct ospf_header_common{
+	//char version;
+	char type;
+	short msg_length;
+	int source_ip_addr;
+	//int area_id;
+	//short checksum;
+	//short authentication_type;
+	//int* authentication_array_start;
+} ospf_header_common; 
+
+typedef struct ospf_hello{
+	ospf_header_common header;
+	int network_mask;
+	short hello_interval;
+	char options;
+	char priority;
+	int router_dead_interval;
+	//int desginated_router_ip;
+	//int desginated_router_backup;
+	int* neighbor_list_start;
+} ospf_header_hello;
+
+typedef struct ospf_header_lsa{
+	short ls_age;
+	short ls_type;
+	int ls_id;
+	int ad_router;
+	int ls_seq_num;
+	short ls_checksum;
+	short ls_length;
+} ospf_header_lsa;
+
+typedef struct ospf_lsrequest{
+	ospf_header_common common_header;	
+	int ls_type;
+	int link_id;
+	int ad_router_ip;
+} ospf_lsrequest;
+
+typedef struct ospf_ad{
+	int link_id;
+	int local_data;
+	char link_type;
+} ospf_ad;
+
+typedef struct ospf_lsupdate{
+	ospf_header_common common_header;	
+	ospf_header_lsa lsa_header;
+	short num_links;
+	ospf_ad* ads;
+} ospf_lsupdate;
+
+
